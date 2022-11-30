d1f790995635c3f487a8e131f096af54ebfb77eb
BLACKNINJA007 <torojonah05@gmail.com>
2022-11-30 15:34:16 -0800

    Correction Updates

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
+# Author - Bamidele Adefolaju
+
 def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
      if a < b:
-          return c
-       elif c > b:
-            return a + b
-         else:
-             return (a * b) - c
-
-# see byte code by uncommenting next two lines and running ./<filename>
-# import dis
-# print(dis.dis(magic_calculation))
-
+          return (c)
+      if c > b:
+          return (a + b)
+      return (a*b - c)
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
+# Author - Tolulope Fakunle
+
 def print_last_digit(number):
-    print(abs(number) % 10, end='')
-     return(abs(number) % 10)
-
- #    if number < 0:
- #        number = number * -1
- #    print("{}".format(number % 10), end='')
- #    return (number % 10)
-
+     print(abs(number) % 10, end="")
+      return (abs(number) % 10)
