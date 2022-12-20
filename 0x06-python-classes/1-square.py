716eee0cecc477f65b0fdbcb73919390e057bc81
BLACKNINJA007 <torojonah05@gmail.com>
2022-12-20 14:20:29 -0800

    correction

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-# 0-square.py by Ehoneah Obed
-"""Defines a square """
-
+"""Defines the square class"""
 
 class Square:
-     """Represents a square"""
-
-      def __init__(self, size):
-           """Initializing this square class
-           Args: size - represnets the size of the square defined
-            """
-
-            self.__size = size
+    """Square class. Has a size"""
+    def __init__(self, sizevar):
+        self.__size = sizevar
